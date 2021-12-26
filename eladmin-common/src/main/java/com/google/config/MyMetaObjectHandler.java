42dbe34ef9b8b185ad5845e2c222017ee9f4c2d8
baixp <baixp@dustess.com>
2021-12-26 17:25:40 +0800

    feat: 测试接口权限，及遗留bug：修改时间和更新人 并不会自动修改

--- eladmin-common/src/main/java/com/google/config/MyMetaObjectHandler.java
+++ eladmin-common/src/main/java/com/google/config/MyMetaObjectHandler.java
@@ -17,13 +17,12 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         this.strictInsertFill(metaObject, "createTime", Timestamp.class, new Timestamp(System.currentTimeMillis()));
-        this.strictInsertFill(metaObject, "updateTime", Timestamp.class, new Timestamp(System.currentTimeMillis()));
         this.strictInsertFill(metaObject, "createBy", String.class, StringUtils.isBlank(SecurityUtils.getCurrentUsername()) ? "System" : SecurityUtils.getCurrentUsername());
-        this.strictInsertFill(metaObject, "updateBy", String.class, StringUtils.isBlank(SecurityUtils.getCurrentUsername()) ? "System" : SecurityUtils.getCurrentUsername());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
+        // TODO 仅第一次生效 数据库值不为null时，并不会触发
         this.strictUpdateFill(metaObject, "updateTime", Timestamp.class, new Timestamp(System.currentTimeMillis()));
         this.strictUpdateFill(metaObject, "updateBy", String.class, StringUtils.isBlank(SecurityUtils.getCurrentUsername()) ? "System" : SecurityUtils.getCurrentUsername());
     }
