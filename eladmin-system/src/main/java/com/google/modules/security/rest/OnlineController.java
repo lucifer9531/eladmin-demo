d01a4661782e1d8eb8dc422e9f9e8f0dd3e22806
baixp <baixp@dustess.com>
2021-12-26 08:27:45 +0800

    feat: 新增在线用户

---
+++ eladmin-system/src/main/java/com/google/modules/security/rest/OnlineController.java
@@ -0,0 +1,40 @@
+package com.google.modules.security.rest;
+
+import com.google.modules.security.service.OnlineUserService;
+import com.google.utils.EncryptUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Set;
+
+/**
+ * @author iris
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth/online")
+@Api(tags = "系统：在线用户管理")
+public class OnlineController {
+
+    private final OnlineUserService onlineUserService;
+
+    @ApiOperation("查询在线用户")
+    @GetMapping
+    public ResponseEntity<Object> query(String filter) {
+        return new ResponseEntity<>(onlineUserService.getAll(filter), HttpStatus.OK);
+    }
+
+    @ApiOperation("踢出用户")
+    @DeleteMapping
+    public ResponseEntity<Object> delete(@RequestBody Set<String> keys) throws Exception {
+        for (String key : keys) {
+            // 解密Key
+            key = EncryptUtils.desDecrypt(key);
+            onlineUserService.kickOut(key);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
